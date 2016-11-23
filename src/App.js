d56627db7bd78ef9ec9b2b5f354de934344b635a
federico hernández velásquez <federico@federicos-MacBook-Pro.local>
2016-11-23 05:47:43 -0300

    Added middle images. Columns need to be fixed.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,38 @@ export default class App extends Component {
     return (
       <main>
           <section>
-            <div className="one"></div>
+            <div className="one">
+              <div className="brandOne">
+                <img src={require('../assets/icon.png')} />
+                <br/>
+                <br/>
+                <img src={require('../assets/NuncheeInteractive.png')} />
+              </div>
+            </div>
           </section>
 
           <section>
-            <div className="two"></div>
+            <div className="two">
+              <div className="container">
+                <div className="col-sm-10 col-sm-offset-1">
+                  <div className="row">
+                    Nuestras apps
+                  </div>
+                  <br></br>
+                  <div className="row">
+                    <div className="col-sm-4">
+                      <img src={require('../assets/kunga.png')} style={{width:'90%'}}/>
+                    </div>
+                    <div className="col-sm-4">
+                      <img src={require('../assets/foto-middle.png')} style={{marginLeft:'20%',width:'60%', borderRadius: '90px'}}/>
+                    </div>
+                    <div className="col-sm-4">
+                      <img src={require('../assets/directtv.png')} style={{width:'90%'}}/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </section>
 
           <section>
@@ -20,7 +47,9 @@ export default class App extends Component {
           </section>
 
           <footer>
-            <div className="four"></div>
+            <div className="four">
+              <img className="brandTwo" src={require('../assets/NuncheeInteractive_White.png')} />
+            </div>
           </footer>
       </main>
     );
